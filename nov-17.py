67389b6ad849617eadb39a13a0ddd176abbd2e4f
madhuprakash23 <33621477+madhuprakash23@users.noreply.github.com>
2017-11-17 15:37:21 +0530

    commit

--- nov-17.py
+++ nov-17.py
@@ -1,2 +1,7 @@
-print("HII")
-
+print("Hi This is my exp on 17-nov")
+a='Hello'
+b="World!!"
+print(a+b)
+list=["Madhu","Tejas","Suhani"]
+print(("Name in the list[2] is::"),list[2])
+print(list[0] * 5)
